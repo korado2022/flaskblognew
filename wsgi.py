571401e13c2523aa5d2a104d154241f27f5fe51a
korado_29 <korado_29@mail.ru>
2023-05-01 14:23:27 +0300

    fix wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,15 @@
+from flask import render_template, url_for
+from werkzeug.utils import redirect
+
 from blog.app import create_app
 
 
 app = create_app()
 
 
-
+@app.route("/")
+def index():
+    return redirect(url_for('article.article_list'))
 
 if __name__ == "__main__":
     app.run(
